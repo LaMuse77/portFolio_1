e39af8e2f0d4ccf5e9a6734c1fabd5de9024932a
LaMuse <quifauri.coulibaly24@gmail.com>
2025-08-25 21:20:50 -0500

    Addaccounts

--- backend/portfolio/portfolio/settings.py
+++ backend/portfolio/portfolio/settings.py
@@ -37,18 +37,29 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
     'Worker_1',
+    'rest_framework',
+    'corshearders',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
 
 ROOT_URLCONF = 'portfolio.urls'
 
